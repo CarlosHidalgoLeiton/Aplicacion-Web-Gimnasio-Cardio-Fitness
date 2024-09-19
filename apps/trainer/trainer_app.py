a127aef2ec97e980b533ffb65394cfaa94f82d91
Halfaro70335 <halfaro70335@ufide.ac.cr>
2024-09-18 21:54:53 -0600

    arreglos y rutas para trainer

--- apps/trainer/trainer_app.py
+++ apps/trainer/trainer_app.py
@@ -4,5 +4,21 @@ trainer_app = Blueprint('trainer_app', __name__)
 
 @trainer_app.route("/")
 def inicio():
-    return render_template("/hola")
+    return render_template("trainer/index.html")
 
+#-------------Rutas de Clientes-------------#
+@trainer_app.route("/clientes")
+def clientes():
+    return render_template("trainer/clientes.html")
+
+
+#-------------Rutas de Rutinas-------------#
+@trainer_app.route("/rutinas")
+def rutinas():
+    return render_template("trainer/rutinas.html")
+
+
+#-------------Rutas de Rutinas-------------#
+@trainer_app.route("/sesiones")
+def sesiones():
+    return render_template("trainer/rutinas.html")
