911ed084e9f58481188b3d6e6a1e8fbae580388c
KennethUN <knavarro60492@ufide.ac.cr>
2024-09-19 20:54:03 -0600

    Cambios rutas del cliente

--- apps/client_app.py
+++ apps/client_app.py
@@ -10,19 +10,36 @@ def inicio():
     return render_template("client/index.html")
 
 
+#-------------Rutas de inventario-------------#
+@client_app.route("/inventario")
+def inventario():
+    return render_template("client/inventario.html")
 
-#-------------Rutas de Rutinas-------------#
-@client_app.route("/rutinas")
+
+#-------------Rutas de rutinas-------------#
+@client_app.route("/rutinaCliente")
 def rutina():
     return render_template("client/rutinaCliente.html")
 
 
-@client_app.route("/rutinas/ver")
+@client_app.route("/verRutina")
 def verRutina():
-    return render_template("client/verRutinaCliente.html")
+    return render_template("/client/verRutinaCliente.html")
 
-#-------------Rutas de Productos-------------#
+@client_app.route("/verSesion")
+def verSesion():
+    return render_template("/client/verSesionClient.html")
 
-@client_app.route("/verProducto")
-def producto():
-    return render_template("/verProducto.html")
+#-------------Rutas de estadisticas-------------#
+@client_app.route("/estadisticas")
+def estadisticas():
+    return render_template("/client/Estadisticas.html")
+
+@client_app.route("/verEstadisticas")
+def verEstadisticas():
+    return render_template("/client/verEstadistica.html")
+
+#-------------Rutas de perfil-------------#
+@client_app.route("/perfil")
+def perfil():
+    return render_template("/client/perfil.html")
