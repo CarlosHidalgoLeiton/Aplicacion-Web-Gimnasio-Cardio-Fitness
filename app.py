0c27af07f3242e3044c3e1fea5636233db3d5f09
CarlosHidalgoLeiton <chidalgo50112@ufide.ac.cr>
2024-09-24 17:53:59 -0600

    Rutas login

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from apps.admin_app import admin_app
 from apps.client_app import client_app
 from apps.trainer_app import trainer_app
+from apps.login_app import login_app
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(admin_app, url_prefix='/admin')
 app.register_blueprint(client_app, url_prefix='/client')
 app.register_blueprint(trainer_app, url_prefix='/trainer')
+app.register_blueprint(login_app)
 
 
 if __name__ == '__main__':
--- apps/login_app.py
+++ apps/login_app.py
@@ -4,5 +4,14 @@ login_app = Blueprint('login_app', __name__)
 
 @login_app.route("/")
 def inicio():
-    return render_template("/hola")
+    return render_template("login/login.html")
 
+
+@login_app.route("/recuperarContrasena")
+def recuperarContrasena():
+    return render_template("login/recuperarContrasena.html")
+
+
+@login_app.route("/cambiarContrasena")
+def cambiarContrasena():
+    return render_template("login/cambiarContrasena.html")
