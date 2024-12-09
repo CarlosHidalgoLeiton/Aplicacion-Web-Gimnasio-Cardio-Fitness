165fcd149a06c2229eb22c05b8d9c1f8703cacca
KennethUN <knavarro60492@gmail.com>
2024-12-09 14:27:28 -0600

    Cambios

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ app.register_blueprint(trainer_app, url_prefix='/trainer')
 app.register_blueprint(login_app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000) 
