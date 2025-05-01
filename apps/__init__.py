3a6f24b9a37a2c79945b70f5d88b33fb474aaf5f
CarlosHidalgoLeiton <carloshl349@gmail.com>
2025-05-01 00:04:04 -0600

    Inicio de sesión

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,24 +1,57 @@
 from flask import Flask, current_app
+from flask_login import LoginManager, current_user
+from flask_principal import Principal, Identity, RoleNeed, identity_changed
 from apps.db.db import db
 from apps.routes.login_app import login_app
 from apps.routes.admin_app import admin_app
 from apps.routes.client_app import client_app
 from apps.routes.trainer_app import trainer_app
+from apps.db.repositories.UserRepository import UserRepository
 
-def create_app():
-    app = Flask(__name__)
+# --- Inicialización ---
+app = Flask(__name__)
+app.config.from_object('config.Config')
+app.secret_key = 'your_secret_key'
 
-    # Configuración de la base de datos
-    app.config.from_object('config.Config')
-    app.secret_key = 'your_secret_key'
+# --- Extensiones ---
+db.init_app(app)
 
-    # Inicializa la instancia de SQLAlchemy con la app
-    db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-    # Registra los blueprints
-    app.register_blueprint(login_app)
-    app.register_blueprint(admin_app, url_prefix='/admin')
-    app.register_blueprint(client_app, url_prefix='/client')
-    app.register_blueprint(trainer_app, url_prefix='/trainer')
+principal = Principal()
+principal.init_app(app)
 
-    return app
+# --- Blueprints ---
+app.register_blueprint(login_app)
+app.register_blueprint(admin_app, url_prefix='/admin')
+app.register_blueprint(client_app, url_prefix='/client')
+app.register_blueprint(trainer_app, url_prefix='/trainer')
+
+# --- User Loader ---
+user_cache = {}
+
+@login_manager.user_loader
+def load_user(user_id):
+    userRepository = UserRepository()
+    if user_id in user_cache:
+        return user_cache[user_id]
+    
+    user = userRepository.get_one(user_id)
+    if user:
+        user_cache[user_id] = user
+    return user
+
+# --- Identity Loader ---
+@principal.identity_loader
+def load_identity():
+    if current_user.is_authenticated:
+        identity = Identity(current_user.ID_Usuario)
+        identity.provides.add(RoleNeed(current_user.Rol))
+        print(f"Identidad cargada: {identity}")
+        identity_changed.send(current_app._get_current_object(), identity=identity)
+        return identity
+
+# --- Ejecutar ---
+if __name__ == '__main__':
+    app.run(debug=True)
